90305988eba832499b63c093fcbf1ce872d1877c
nakyeong <yunna3425@naver.com>
2026-05-16 02:45:51 +0900

    chore: 디자인 시스템 정의 (#6)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Providers from './providers';
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: 'Pawsture',
   description: '반려견 슬개골 탈구 방지 걸음 분석 서비스',
 };
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  display: 'swap',
+});
 
 const RootLayout = ({
   children,
@@ -13,9 +20,9 @@ const RootLayout = ({
   children: React.ReactNode;
 }>): React.ReactElement => {
   return (
-    <html lang="ko" className="h-full antialiased">
-      <body className="vsc-initialized flex min-h-full flex-col bg-neutral-100">
-        <div className="mx-auto flex w-full max-w-107.5 flex-1 flex-col bg-white">
+    <html lang="ko" className={`${pretendard.variable} h-full`}>
+      <body className="font-pretendard flex h-full flex-col bg-neutral-100 antialiased">
+        <div className="mx-auto flex min-h-full w-full max-w-107.5 flex-1 flex-col bg-white">
           <Providers>{children}</Providers>
         </div>
       </body>
